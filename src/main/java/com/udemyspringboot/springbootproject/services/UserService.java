6c9229aee82f054ed2dfcd40925c899d5d662f46
Pedro Garbin <plgbpedro2@gmail.com>
2025-01-19 15:34:37 -0300

    Service layer, component registration

---
+++ src/main/java/com/udemyspringboot/springbootproject/services/UserService.java
@@ -0,0 +1,30 @@
+package com.udemyspringboot.springbootproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemyspringboot.springbootproject.entities.User;
+import com.udemyspringboot.springbootproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		
+		return userRepository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+	}
+	
+}
