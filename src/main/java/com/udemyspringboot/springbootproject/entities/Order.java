10ce00c6636d7788363450707cdfbc1fa2cce1fc
Pedro Garbin <plgbpedro2@gmail.com>
2025-01-19 17:46:05 -0300

    OrderStatus enum

--- src/main/java/com/udemyspringboot/springbootproject/entities/Order.java
+++ src/main/java/com/udemyspringboot/springbootproject/entities/Order.java
@@ -2,12 +2,14 @@ package com.udemyspringboot.springbootproject.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
+import java.util.Objects;import org.apache.tomcat.util.net.openssl.OpenSSLStatus.Name;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.udemyspringboot.springbootproject.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +29,8 @@ public class Order implements Serializable {
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -35,9 +38,10 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,6 +59,18 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
